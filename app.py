0b36cfe8dc0f8c97749b6c80809953616f905253
countBunny <happy_zoo@126.com>
2018-03-26 17:52:02 +0800

    app been created

---
+++ app.py
@@ -0,0 +1,20 @@
+import asyncio, os, json, time
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+from datetime import datetime
+
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h>')
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
